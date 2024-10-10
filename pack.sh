a3e16f76adef5e8dc772f2f2f654bd7c65fcf6e3
Vincent DARON <vda@dfakto.com>
2024-10-10 13:50:56 +0200

    Fix typo

--- pack.sh
+++ pack.sh
@@ -2,7 +2,7 @@
 
 dotnet build -c Release -p:TargetFrameworks="net6.0" -p:PackageVersion=$1 -p:Version=$1 -p:FileVersion=$1 ASK.HAL/ASK.HAL.csproj 
 dotnet build -c Release -p:TargetFrameworks="net6.0" -p:PackageVersion=$1 -p:Version=$1 -p:FileVersion=$1 ASK.HAL.Mvc/ASK.HAL.Mvc.csproj
-dotnet build -c Release -p:TargetFrameworks="net6.0" -p:PackageVersion=$1 -p:Version=$1 -p:FileVersion=$1 ASK.HAL/ASK.HAL.csproj 
+dotnet build -c Release -p:TargetFrameworks="net8.0" -p:PackageVersion=$1 -p:Version=$1 -p:FileVersion=$1 ASK.HAL/ASK.HAL.csproj
 dotnet build -c Release -p:TargetFrameworks="net8.0" -p:PackageVersion=$1 -p:Version=$1 -p:FileVersion=$1 ASK.HAL.Mvc/ASK.HAL.Mvc.csproj
 
 rm -rf nuget
